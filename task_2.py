671fc767b7259e3054341c4e3c80687fef5a5d1e
sergei sam <ssi1970@mail.ru>
2022-09-06 09:32:29 +0700

    correction

--- task_2.py
+++ task_2.py
@@ -1,24 +1,10 @@
 # Напишите программу для. проверки истинности утверждения
 # ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-def inputNumbers(x):
-    xyz = ["X", "Y", "Z"]
-    a = []
-    for i in range(x):
-        a.append(input(f"Введите значение {xyz[i]}: "))
-    return a
 
 
-def checkPredicate(x):
-    left = not (x[0] or x[1] or x[2])
-    right = not x[0] and not x[1] and not x[2]
-    result = left == right
-    return result
-
-
-statement = inputNumbers(3)
-
-if checkPredicate(statement) == True:
-    print(f"Утверждение истинно")
-else:
-    print(f"Утверждение ложно")
+for X in range(0,2):
+   for Y in range(0,2):
+    for Z in range(0,2): 
+        print(f'{X} {Y} {Z}')
+print(f'{(not(X or Y or Z)) == (not X and not Y and not Z)}')
